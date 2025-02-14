afee5eb63b8706669dcad6902d2f763c9dcfe179
kyungwon-kang <kkw.theo@gmail.com>
2025-02-14 18:03:06 +0900

    주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -6,6 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Repository
 @RequiredArgsConstructor
@@ -37,5 +41,30 @@ public class OrderQueryRepository {
                 " from orders o" +
                 " join o.member m" +
                 " join o.delivery d", OrderQueryDto.class).getResultList();
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        List<Long> orderIds = toOrderIds(result);
+
+        Map<Long, List<OrderItemQueryDto>> orderItemsMap = findOrderItemMap(orderIds);
+
+        result.forEach(o -> o.setOrderItems(orderItemsMap.get(o.getOrderId())));
+
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery("select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)  " +
+                " from OrderItem oi" +
+                " join oi.item i" +
+                " where oi.order.id in :orderIds", OrderItemQueryDto.class).setParameter("orderIds", orderIds).getResultList();
+
+        return orderItems.stream().collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    private static List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream().map(OrderQueryDto::getOrderId).collect(toList());
     }
 }
